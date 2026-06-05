f8b9287f3d46236d5ff9cdefd46837970d7642e2
ujwalraj58 <ujwalrajdussa@gmail.com>
2026-06-05 11:27:54 +0530

    1

--- college_portal/settings.py
+++ college_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-a%%kgq(p1nn(*55=n3%#-7^0n*0*h=gb=pjdvc2fh7x6cl=gol
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',
+                 'onrender.com',
+                 'localhost',
+                 '127.0.0.1',
+            ]
 
 
 # Application definition
@@ -118,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
